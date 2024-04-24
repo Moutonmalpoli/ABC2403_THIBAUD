55998554861e6ab4725854d4c63e1ddd40831b65
Moutonmalpoli <thibaud.clairet@hotmail.fr>
2024-04-24 16:32:13 +0200

    Exercice helloYou Java

---
+++ 03-algorithme/helloYou/src/helloYou/App.java
@@ -0,0 +1,26 @@
+package helloYou;
+
+import java.util.Scanner; // importer des composants
+
+public class App {
+
+	public static void main(String[] args) {
+	 
+		String prenom;
+		
+		Scanner sc; // Déclaration d'une variable de ty pe Scanner (penser à la ligne import (ligne 3))
+		
+		sc = new Scanner(System.in); // initialisation d'un nouveau Scanner
+		
+		System.out.println("Bonjour, veuillez saisir votre prénom"); //Affichage
+		
+		prenom = sc.nextLine(); // Demande a l'utilisateur une saisie. 
+		//Lorsque l'utilisateur validera avec la touche entrée, la saisie est récupérée dans la variable à gauche du signe "="
+		
+		System.out.println("Bonjour " + prenom);
+		
+		sc.close(); // Fermeture du Scanner (obligatoire)
+
+	}
+
+}
