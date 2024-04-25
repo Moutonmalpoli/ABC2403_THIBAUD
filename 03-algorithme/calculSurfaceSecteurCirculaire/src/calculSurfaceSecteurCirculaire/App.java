4a922d1aa53c1af6ee45309e3609c513d4237393
Moutonmalpoli <thibaud.clairet@hotmail.fr>
2024-04-25 12:54:58 +0200

    Exercice 1.5

--- 03-algorithme/calculSurfaceSecteurCirculaire/src/calculSurfaceSecteurCirculaire/App.java
+++ 03-algorithme/calculSurfaceSecteurCirculaire/src/calculSurfaceSecteurCirculaire/App.java
@@ -1,10 +1,31 @@
 package calculSurfaceSecteurCirculaire;
+import java.util.Scanner;
+import java.lang.Math;
 
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		int rayonR;
+		int angleA;
+		double aireCirculaire;
+		Scanner sc;
+		
+		sc = new Scanner (System.in);
+		
+		System.out.println("Saisir le rayon du cercle: ");
+		
+		rayonR = sc.nextInt();
+		
+		System.out.println("Saisir l'angle A: ");
+		
+		angleA = sc.nextInt();
+		
+		aireCirculaire = (Math.PI * Math.pow(rayonR, 2) * angleA ) /360;
+				
+		System.out.println("L'aire circulaire du rayon " + rayonR + "et de l'angle " + angleA + "Est:" + aireCirculaire + ".");
+		
+		sc.close();
 	}
 
 }
