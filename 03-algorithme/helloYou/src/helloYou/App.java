17fc2c4c868be80634b17b0e3373b9b768e15693
Moutonmalpoli <thibaud.clairet@hotmail.fr>
2024-04-25 09:26:38 +0200

    modification

--- 03-algorithme/helloYou/src/helloYou/App.java
+++ 03-algorithme/helloYou/src/helloYou/App.java
@@ -11,7 +11,7 @@ public class App {
 		Scanner sc; // Déclaration d'une variable de ty pe Scanner (penser à la ligne import (ligne 3))
 		
 		sc = new Scanner(System.in); // initialisation d'un nouveau Scanner
-		
+		// A la création du Scanner, on lui injecte System.in indiquant qu'on souhaite récupérer les données à partir de ce flux
 		System.out.println("Bonjour, veuillez saisir votre prénom"); //Affichage
 		
 		prenom = sc.nextLine(); // Demande a l'utilisateur une saisie. 
