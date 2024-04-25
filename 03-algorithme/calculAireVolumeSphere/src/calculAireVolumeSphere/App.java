79327815fb434d69a420ca0047e2e311eeb59bbf
Moutonmalpoli <thibaud.clairet@hotmail.fr>
2024-04-25 12:55:03 +0200

    Update App.java

--- 03-algorithme/calculAireVolumeSphere/src/calculAireVolumeSphere/App.java
+++ 03-algorithme/calculAireVolumeSphere/src/calculAireVolumeSphere/App.java
@@ -1,11 +1,14 @@
 package calculAireVolumeSphere;
+
 import java.util.Scanner;
+import java.lang.Math;
 public class App {
 
 	public static void main(String[] args) {
 		
 		int rayonR;
-		
+		double aire;
+		double volume;
 		Scanner sc;
 
 		sc = new Scanner (System.in);
@@ -14,14 +17,18 @@ public class App {
 		
 		rayonR = sc.nextInt();
 		
+		
+		aire = (4 * Math.PI * Math.pow(rayonR, 2));
+		volume = (4 * Math.PI * Math.pow (rayonR, 3)) / 3;
+		
+		System.out.println("");		
+		System.out.println("L'aire d'une sphère de rayon " + rayonR + " est :" +  aire +  ".");
+		
+		System.out.println("");
+		System.out.println("Le volume d'une sphère de rayon " + rayonR + " est :" +  volume +  ".");
+		
 		sc.close();
-		
-		double aire = (4 * 3.14 * rayonR * rayonR);
-		double volume = (4 * 3.14 * rayonR * rayonR * rayonR) / 3;
-		
-		System.out.println(" L'aire d'une sphère de rayon " + rayonR + " est :" +  aire +  ".");
-		System.out.println(" Le volume d'une sphère de rayon " + rayonR + " est :" +  volume +  ".");
-		
+
 	}
 
 }
