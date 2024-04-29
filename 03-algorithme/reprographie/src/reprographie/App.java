53c79c60b528305b1adfd7f29ea6c865243bebb8
Moutonmalpoli <thibaud.clairet@hotmail.fr>
2024-04-29 16:18:32 +0200

    Exercice 2.5 java

---
+++ 03-algorithme/reprographie/src/reprographie/App.java
@@ -0,0 +1,35 @@
+package reprographie;
+import java.util.Scanner;
+public class App {
+
+	public static void main(String[] args) {
+
+		int nbrPhotocopies;
+		double facture;
+		double factureClient;
+		Scanner sc;
+		
+		sc = new Scanner(System.in);
+		
+		System.out.println("Veuillez indiquer le nombre de photocopies.");
+		
+		nbrPhotocopies = sc.nextInt();
+		sc.close();
+		
+		facture = 0.10;
+		
+		if (nbrPhotocopies > 10 && nbrPhotocopies <= 19){
+			facture = 0.09;
+		}
+		else if (nbrPhotocopies >20  ) {
+			facture = 0.08;
+		}
+		else {
+		facture = 0.10;
+		}
+		factureClient = facture * nbrPhotocopies;
+		
+		System.out.println("Pour " + nbrPhotocopies + " photocopies votre facture sera de " + factureClient + " euros" + ".");
+	}
+
+}
