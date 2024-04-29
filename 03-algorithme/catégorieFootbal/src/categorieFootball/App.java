4f6f0aadabfa4185236dd52b7cb1db32ade0ffa4
Moutonmalpoli <thibaud.clairet@hotmail.fr>
2024-04-29 16:05:17 +0200

    Update App.java

--- 03-algorithme/catégorieFootbal/src/categorieFootball/App.java
+++ 03-algorithme/catégorieFootbal/src/categorieFootball/App.java
@@ -16,25 +16,35 @@ public class App {
 		if (ageUser < 5) {
 			System.out.println( "Vous êtes trop jeune");
 		
-		}
-			else if (ageUser == 5 || ageUser == 6)
+			}
+			else if (ageUser == 5 || ageUser == 6) {
 				System.out.println( "Vous êtes débutant");
-			else if (ageUser == 7 || ageUser == 8)
+			}
+			else if (ageUser == 7 || ageUser == 8) {
 				System.out.println( "Vous êtes poussin");
-		 	else if (ageUser == 9 || ageUser == 10)
-		 		System.out.println("Vous êtes benjamin");
-		 	else if (ageUser == 11 || ageUser == 12)
+			}
+		 	else if (ageUser == 9 || ageUser == 10) {
+		 			System.out.println("Vous êtes benjamin");
+		 	}
+		 	else if (ageUser == 11 || ageUser == 12) {
 		 		System.out.println( "Vous êtes pupille");
-		 	else if (ageUser == 13 || ageUser == 14)
+		 	}
+		 	else if (ageUser == 13 || ageUser == 14) {
 		 		System.out.println( "Vous êtes minime");
-		 	else if (ageUser == 15 || ageUser == 16)	
+		 	
+		 	}	
+		 	else if (ageUser == 15 || ageUser == 16) {	
 		 		System.out.println("Vous êtes cadet");
-		 	else if (ageUser == 17 || ageUser == 18)	
+		 	}
+		 	else if (ageUser == 17 || ageUser == 18) {	
 		 		System.out.println("Vous êtes junior");
-		 	else if (ageUser >= 19 && ageUser <= 34)	
+		 	}
+		 	else if (ageUser >= 19 && ageUser <= 34) {	
 		 		System.out.println("Vous êtes sénior");
-		 else
+		 	}
+		 else {
 			System.out.println("Vous êtes vétéran");
+		 }
 			 
 	
 	}
