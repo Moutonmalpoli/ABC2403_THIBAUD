fa58a9bf8b8dfe10f7a8ebb7f61282598741eba1
Moutonmalpoli <thibaud.clairet@hotmail.fr>
2024-04-30 09:54:06 +0200

    Exercice 2.6 java

---
+++ 03-algorithme/elections/src/elections/App.java
@@ -0,0 +1,50 @@
+package elections;
+import java.util.Scanner;
+public class App {
+
+	public static void main(String[] args) {
+		double scoreCandidat1;
+		double scoreDesAutres;
+		double scoreTotal;
+		Scanner sc;
+		
+		sc= new Scanner (System.in);
+		
+		System.out.println("Saisir le score du candidat n°1");
+		scoreCandidat1 = sc.nextDouble() ;
+		
+		System.out.println("Saisir le score des autres candidats");
+		scoreDesAutres = sc.nextDouble();
+		sc.close();
+		
+		scoreTotal = scoreCandidat1 + scoreDesAutres;
+		
+		if (scoreTotal != 100) {
+			System.out.println("Le score doit être égal à 100%");
+			
+		}
+		else {
+			System.out.println("Le score du candidat 1 est de " + scoreCandidat1 + "%");
+		}	
+		
+		if (scoreCandidat1 > 50 && scoreDesAutres <= 50) {
+			System.out.println("Le candidat 1 est élu.");
+		}
+		else if (scoreCandidat1 < 50 && scoreCandidat1 >= 12.5) {
+			System.out.println("Le candidat 1 passe au second tour.");
+		}
+		else if(scoreCandidat1 < 12.5) {
+			System.out.println("Le candidat 1 est éliminé.");
+		}
+		else if(scoreCandidat1 > scoreDesAutres) {
+			System.out.println("Le candidat1 est en ballotage favorable.");
+			}
+		else if (scoreCandidat1 < scoreDesAutres) {
+			System.out.println("Le candidat 1 est en ballotage défavorable.");
+		}
+		else {
+			System.out.println("Score invalide");
+		}
+		
+		}
+	}
