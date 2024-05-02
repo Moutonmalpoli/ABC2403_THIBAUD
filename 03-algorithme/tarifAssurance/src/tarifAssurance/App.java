e88eb87e9448ca78343d3e6c430bfefdff6f5292
Moutonmalpoli <thibaud.clairet@hotmail.fr>
2024-05-02 15:48:14 +0200

    Update App.java

--- 03-algorithme/tarifAssurance/src/tarifAssurance/App.java
+++ 03-algorithme/tarifAssurance/src/tarifAssurance/App.java
@@ -20,17 +20,87 @@ public class App {
 		System.out.println("Depuis combien d'années êtes-vous titulaire du permis ?");
 		permis = sc.nextInt();
 		
-		System.out.println("Depuis combien d'années êtes-vous assuré chez nous ?");
-		assurance = sc.nextInt();
-		
 		System.out.println("De combien d'accidents avez-vous été responsable ?");
 		accidents = sc.nextInt();
 		
+		System.out.println("Depuis combien d'années êtes-vous assuré chez nous ?");
+		assurance = sc.nextInt();
+		
+		
 		System.out.println("Vous avez " + ageConducteur + " ans.");
 		System.out.println("Vous êtes titulaire du permis depuis " + permis + " ans");
-		System.out.println("Vous êtes assuré chez nous depuis" + assurance + " ans");
-		System.out.println("Vous avez été responsable de " + accidents + " accidents");
+		System.out.println("Vous avez été reconnu responsable de " + accidents + " accidents");
+		System.out.println("Vous êtes assuré chez nous depuis " + assurance + " ans");
 		
+		if (ageConducteur < 25 && permis < 2 && accidents == 0 && assurance < 5 ) {
+			System.out.println("Vous bénéficier du tarif rouge");
+		}
+		
+		else if (ageConducteur < 25 && permis < 2 && accidents == 1 && assurance > 5 ) {
+			System.out.println("Vous bénéficier du tarif rouge");
+		}
+		
+		else if (ageConducteur > 25 && permis < 2 && accidents == 1 && assurance < 5 ) {
+			System.out.println("Vous bénéficier du tarif rouge");
+		}
+		
+		else if (ageConducteur > 25 && permis < 2 && accidents == 1 && assurance > 5 ) {
+			System.out.println("Vous bénéficier du tarif rouge");
+		}
+		
+		else if (ageConducteur > 25 && permis > 2 && accidents == 2 && assurance < 5 ) {
+			System.out.println("Vous bénéficier du tarif rouge");
+		}
+		
+		else if (ageConducteur > 25 && permis > 2 && accidents == 3 && assurance > 5 ) {
+			System.out.println("Vous bénéficier du tarif rouge");
+		}
+		
+		else if (ageConducteur < 25 && permis < 2 && accidents == 0 && assurance > 5 ) {
+			System.out.println("Vous bénéficier du tarif orange");
+		}
+		
+		else if (ageConducteur < 25 && permis > 2 && accidents == 0 && assurance < 5 ) {
+			System.out.println("Vous bénéficier du tarif orange");
+		} 
+		
+		else if (ageConducteur > 25 && permis < 2 && accidents == 0 && assurance < 5 ) {
+			System.out.println("Vous bénéficier du tarif orange");
+		} 
+		
+		else if (ageConducteur > 25 && permis > 2 && accidents == 1 && assurance < 5 ) {
+			System.out.println("Vous bénéficier du tarif orange");
+		}	
+		else if (ageConducteur > 25 && permis > 2 && accidents == 2 && assurance > 5 ) {
+				System.out.println("Vous bénéficier du tarif orange");
+			} 
+		else if (ageConducteur > 25 && permis < 2 && accidents == 0 && assurance > 5 ) {
+			System.out.println("Vous bénéficier du tarif vert");
+		} 
+		else if (ageConducteur > 25 && permis > 2 && accidents == 0 && assurance < 5 ) {
+			System.out.println("Vous bénéficier du tarif vert");
+		} 
+		else if (ageConducteur > 25 && permis > 2 && accidents == 1 && assurance > 5 ) {
+			System.out.println("Vous bénéficier du tarif vert");
+		} 
+		else if (ageConducteur > 25 && permis > 2 && accidents == 0 && assurance > 5 ) {
+			System.out.println("Vous bénéficier du tarif bleu");
+		}
+		else if (ageConducteur < 25 && permis < 2 && accidents == 1 && assurance < 5 ) {
+			System.out.println("Votre demande d'assurance est refusée");
+		}
+		else if (ageConducteur < 25 && permis < 2 && accidents >= 1 && assurance > 5 ) {
+			System.out.println("Votre demande d'assurance est refusée");
+		}
+		else if (ageConducteur > 25 && permis < 2 && accidents >= 1 && assurance < 5 ) {
+			System.out.println("Votre demande d'assurance est refusée");
+		}
+		else if (ageConducteur > 25 && permis > 2 && accidents >= 3 && assurance < 5 ) {
+			System.out.println("Votre demande d'assurance est refusée");
+		}
+		else if (ageConducteur > 25 && permis > 2 && accidents >= 4 && assurance > 5 ) {
+			System.out.println("Votre demande d'assurance est refusée");
+		}
 		sc.close();
 	}
 
