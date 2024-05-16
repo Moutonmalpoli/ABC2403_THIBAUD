5ca14ce69a1ebcda9ffc5c2e8b05359bfca2d45a
Moutonmalpoli <thibaud.clairet@hotmail.fr>
2024-05-16 09:53:44 +0200

    Update App.java

--- 03-algorithme/plageDeSable/src/plageDeSable/App.java
+++ 03-algorithme/plageDeSable/src/plageDeSable/App.java
@@ -4,22 +4,30 @@ public class App {
 
 	public static void main(String[] args) {
 		double nombreUser;
-		Scanner sc = new Scanner(System.in);
+		double nombreReponse;
+		Scanner sc;
+		
+		sc = new Scanner(System.in);
+		
+		nombreReponse = 1.7 ;
 
 		System.out.println("Saisir un nombre compris entre 1 et 3");
 		nombreUser = sc.nextDouble();
 		
-		while (nombreUser != 1.7) {
-			
-			if (nombreUser == 1.7) {
-				System.out.println("Bravo, vous avez réussi");	
-				}
-				else {
-					System.out.println("Perdu, réessayez");
-					nombreUser = sc.nextDouble();
-				}
-		}
 		
+		while (nombreUser != nombreReponse ) {
+			
+			System.out.println("Perdu, réessayez");
+			nombreUser = sc.nextDouble();
+		}		
+		if (nombreUser == nombreReponse ) {
+			System.out.println(nombreUser + " Bravo, vous avez réussi");	
+			}
+			else {
+				System.out.println("Perdu, réessayez");
+				}
+		sc.close();
+
 		}
 	}
 
