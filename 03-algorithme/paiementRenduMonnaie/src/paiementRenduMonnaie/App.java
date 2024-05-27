eb9e22e90c15dafac35b730db2a16a64bd2b7a41
Moutonmalpoli <thibaud.clairet@hotmail.fr>
2024-05-27 12:54:39 +0200

    Update App.java

--- 03-algorithme/paiementRenduMonnaie/src/paiementRenduMonnaie/App.java
+++ 03-algorithme/paiementRenduMonnaie/src/paiementRenduMonnaie/App.java
@@ -14,14 +14,20 @@ public class App {
 		 int nbrPiece050;
 		 int nbrPiece1;
 		 int nbrPiece2;
+		 
+		 nbrPiece010 = 0;
+		 nbrPiece020 = 0;
+		 nbrPiece050 = 0;
+		 nbrPiece1 = 0;
+		 nbrPiece2 = 0;
 		
 		 prixArticle = 1;
 		 sommeTotale = 0;
 		 totalBillets = 0;
 		 nombreBillets = 0;
-		 rendueMonnaie = sommeTotale - totalBillets;
-		 nombreBillets =   ;
-		 totalBillets = nombreBillets * 5;
+		 rendueMonnaie = 0;
+
+
 		 
 		 Scanner sc;
 		 
@@ -34,12 +40,52 @@ public class App {
 			 sommeTotale = sommeTotale + prixArticle;
 			 
 		 }
-		 System.out.println("Le client doit payer " + sommeTotale + " Euros.");
-		 System.out.println("Le client doit donner " + nombreBillets + "  de 5 euros soit " + totalBillets );
+		 System.out.printf("Le client doit payer %.2f euros"  , sommeTotale);
+		 System.out.println();
+		 
+		 while(sommeTotale >= 5) {
+			
+			 nombreBillets++;
+			 sommeTotale = sommeTotale -5;
+		 }
+		 
+		 totalBillets = nombreBillets * 5;
+		 rendueMonnaie = sommeTotale;
+		 
+		 
+		 System.out.println("Le client doit donner " + nombreBillets + " billets de 5 euros soit " + totalBillets );
 		 System.out.println("");
-		 System.out.println("Rendue Monnaie: " + rendueMonnaie + " euros");
+		 System.out.printf("Rendue Monnaie: %.2f euros " , rendueMonnaie);
 		 
+		 while (rendueMonnaie >= 2) {
+			 rendueMonnaie = rendueMonnaie - 2;
+			 nbrPiece2++;
+		 }
+		 while (rendueMonnaie >= 1) {
+			 rendueMonnaie = rendueMonnaie - 1;
+			 nbrPiece1++;
+		 }
+		 while (rendueMonnaie >= 0.50) {
+			 rendueMonnaie = rendueMonnaie - 0.50;
+			 nbrPiece050++;
+		 }
+		 while (rendueMonnaie >= 0.20) {
+			 rendueMonnaie = rendueMonnaie - 0.20;
+			 nbrPiece020++;
+		 }
+		 while (rendueMonnaie >= 0.10) {
+			 rendueMonnaie = rendueMonnaie - 0.10;
+			 nbrPiece010++;
+		 }
+		 System.out.println("");
+		 System.out.println("Répartition de la monnaie à restituer au client : ");
+		 System.out.println(nbrPiece2 + " pièce de 2 euros");
+		 System.out.println(nbrPiece1 + " pièce de 1 euros");
+		 System.out.println(nbrPiece050 + " pièce de 0,50 euros");
+		 System.out.println(nbrPiece020 + " pièce de 0,20 euros");
+		 System.out.println(nbrPiece010 + " pièce de 0,10 euros");
 		 
+	sc.close();	 
 	}
 
 }
