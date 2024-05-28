cffc6786a556b2df95e2141f21aab396f725475e
Moutonmalpoli <thibaud.clairet@hotmail.fr>
2024-05-28 15:24:20 +0200

    demo Fonction 2

---
+++ 03-algorithme/demoFonctions2/src/demoFonctions2/App.java
@@ -0,0 +1,18 @@
+package demoFonctions2;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		int a;
+		a = 20;
+		App.changerValeur(a);
+		System.out.println(a);
+	}
+	
+	public static void changerValeur(int a) {
+		a = 30;
+
+}
+	
+}
---
+++ 03-algorithme/demoFonctions2/src/demoFonctions2/toto.java
@@ -0,0 +1,10 @@
+package demoFonctions2;
+
+public class toto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
