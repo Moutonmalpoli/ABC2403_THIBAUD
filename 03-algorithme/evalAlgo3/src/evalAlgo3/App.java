d42174add4d3979d2cb47ed3928b775382c5e0d6
Moutonmalpoli <thibaud.clairet@hotmail.fr>
2024-06-03 12:20:23 +0200

    evalAlgo3

---
+++ 03-algorithme/evalAlgo3/src/evalAlgo3/App.java
@@ -0,0 +1,31 @@
+package evalAlgo3;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		int[] numbers;
+		float moyenne;
+		float somme;
+		int pgv;
+		int pgvCarré;
+		int i;
+
+		numbers = new int[] { 2,48, 4, 1, 8, 6, 14, 23, 25, 7, 42 };
+		moyenne = 0;
+		somme = 0;
+		pgv = 0;
+		for (i = 0; i < numbers.length; i++) {
+
+			somme += numbers[i];
+			moyenne = somme / numbers.length;
+			pgv = numbers[i];
+		}
+
+		pgvCarré = pgv * pgv;
+		System.out.println("Bienvenue dans le programme de calcul du tableau d'entier");
+		System.out.println("Moyenne des valeurs du tableau: " + moyenne);
+		System.out.println("Valeur la plus grande " + "(" + pgv + ")" + " élevé au carré :" + pgvCarré);
+
+	}
+}
