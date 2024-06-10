3a03a6f0acc29e51ff1a756146312ecc29594838
Moutonmalpoli <thibaud.clairet@hotmail.fr>
2024-06-10 15:11:06 +0200

    Update App.java

--- Objet/bouteilles/src/bouteilles/App.java
+++ Objet/bouteilles/src/bouteilles/App.java
@@ -3,6 +3,7 @@ package bouteilles;
 public class App {
 
 	public static void main(String[] args) {
+		//Type	nom_Variable = mot_clé	nom_classe (valeur,des, champs)
 		Bouteille bouteille1 = new Bouteille(); 
 		
 		Bouteille bouteille2 = new Bouteille(1.2f, 0.8f, true, "H20");
