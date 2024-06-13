39e5560698de3c8278204f4ea857ab7ee286924e
Moutonmalpoli <thibaud.clairet@hotmail.fr>
2024-06-13 13:44:19 +0200

    Update Bouteille.java

--- Objet/bouteilles/src/bouteilles/Bouteille.java
+++ Objet/bouteilles/src/bouteilles/Bouteille.java
@@ -7,6 +7,11 @@ public class Bouteille {
 	private float contenanceEnL;
 	private float contenuEnL;
 	private boolean ouvert;
+	private boolean fermer;
+	private boolean remplir;
+	private boolean vider;
+	private float quantite;
+	
 	
 	//access/modifier
 	
@@ -14,25 +19,34 @@ public class Bouteille {
 	
 	//constructeur par defaut
 	public Bouteille() {
-		typeDeContenu="H20";
-		contenanceEnL=1;
-		contenuEnL=1;
-		ouvert=false;
+		typeDeContenu= "H20";
+		contenanceEnL= 1;
+		contenuEnL= 1;
+		ouvert= false;
+		fermer= true;
+		remplir= false;
+		vider= false;
+		quantite= 0;
 	}
 	
 	//constructeur classique
 	
-	public Bouteille(float _contenanceEnL,float _contenuEnL,boolean _ouvert,String _typeDeContenu) {
+	public Bouteille(float _contenanceEnL,float _contenuEnL,boolean _ouvert,String _typeDeContenu, boolean _fermer, boolean _remplir, boolean _vider, float _quantite) {
 		
+		// initialization attributes 
 		// this refers to the object, not the class
 		this.contenanceEnL= _contenanceEnL;
 		this.contenuEnL=_contenuEnL;
 		this.ouvert=_ouvert;
 		this.typeDeContenu=_typeDeContenu;
+		this.fermer =_fermer;
+		this.remplir =_remplir;
+		this.vider=_vider;
+		this.quantite= _quantite;
 	}
 	
 	//constructeur par recopie ou de clonage
-	
+	/*
 	public Bouteille(Bouteille _bouteilleARecopier) {
 		this.contenanceEnL=_bouteilleARecopier.contenanceEnL;
 		this.contenuEnL=_bouteilleARecopier.contenuEnL;
@@ -40,6 +54,7 @@ public class Bouteille {
 		this.typeDeContenu=_bouteilleARecopier.typeDeContenu;
 		
 	}
+	*/
 	
 	//methods
 	
@@ -57,16 +72,47 @@ public class Bouteille {
 	public boolean ouvert() {
 		if (ouvert=true) {
 			
-		System.out.println("Impossible, la bouteille est déja ouverte");
-		return ;
+		System.out.println("Impossible, la bouteille est déja ouverte.");
+		return ouvert;
 		}
 		else {
 			
 			ouvert = true;
-			System.out.println("La bouteille est ouverte");
+			System.out.println("La bouteille est ouverte.");
 			return ouvert;
 		}
 	}
-	
+	public boolean fermer() {
+		if (fermer=true) {
+			System.out.println("Impossible, la bouteille est déja fermée.");
+			return fermer;
+		}
+		else {
+			fermer = true;
+			System.out.println("La bouteille est fermée.");
+			return fermer;
+		}
 	}
+
+	public boolean remplir(float quantite) {
+		boolean retour=false;
+
+		if (this.ouvert== true && quantite<=(this.contenanceEnL-this.contenuEnL))
+		{
+			retour = true;
+			contenuEnL+= quantite;
+			
+		}
+
+		return retour;
+		
+	}
+	public boolean vider(float quantite) {
+		boolean retour= false;
+		
+		if(this.ouvert == true && ) {
+			
+		}
+		}
+}
 
